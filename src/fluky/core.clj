7ad4fefd7ccd2270ea5f279f70987236510d0f8a
Mourjo Sen <mourjo@helpshift.com>
2020-11-21 12:07:18 +0530

    MIN MAX quantifier

--- src/fluky/core.clj
+++ src/fluky/core.clj
@@ -5,28 +5,32 @@
 
 (insta/defparser rgx
   "
-REGEX = (ESCAPED | DOT | CHAR | POS_SET | NEG_SET | STAR_QUANTIFIER | PLUS_QUANTIFIER | QMARK_QUANTIFIER)*
+REGEX = (ESCAPED | DOT | CHAR | POS_SET | NEG_SET | STAR_QUANTIFIER | PLUS_QUANTIFIER | QMARK_QUANTIFIER | MIN_MAX_QUANTIFIER)* ;
 
 CHAR = \"a\" | \"A\"| \"b\" | \"B\" | \"c\" | \"C\" | \"d\" | \"D\" | \"e\" | \"E\" | \"f\" | \"F\" |
        \"g\" | \"G\"| \"h\" | \"H\" | \"i\" | \"I\" | \"j\" | \"J\" | \"k\" | \"K\" | \"l\" | \"L\" |
        \"m\" | \"M\"| \"n\" | \"N\" | \"o\" | \"O\" | \"p\" | \"P\" | \"q\" | \"Q\" | \"r\" | \"R\" |
        \"s\" | \"S\"| \"t\" | \"T\" | \"u\" | \"U\" | \"v\" | \"V\" | \"w\" | \"W\" | \"x\" | \"X\" |
        \"y\" | \"Y\"| \"z\" | \"Z\" | \"0\" | \"1\" | \"2\" | \"3\" | \"4\" | \"5\" | \"6\" | \"7\" |
-       \"8\" | \"9\"
+       \"8\" | \"9\" ;
 
-BACK_SLASH = \"\\\\\"
-ESCAPED = (BACK_SLASH \"+\") | (BACK_SLASH \"*\") | (BACK_SLASH \"?\") | (BACK_SLASH \".\")
+BACK_SLASH = \"\\\\\" ;
+ESCAPED = (BACK_SLASH \"+\") | (BACK_SLASH \"*\") | (BACK_SLASH \"?\") | (BACK_SLASH \".\") ;
 
-DOT = \".\"
+DOT = \".\";
 
-RANGE = CHAR \"-\" CHAR
-POS_SET = \"[\" (RANGE | ESCAPED | CHAR | DOT)* \"]\"
-NEG_SET = \"[\" \"^\" (RANGE | ESCAPED | CHAR | DOT)* \"]\"
+RANGE = CHAR \"-\" CHAR ;
+POS_SET = \"[\" (RANGE | ESCAPED | CHAR | DOT)* \"]\" ;
+NEG_SET = \"[\" \"^\" (RANGE | ESCAPED | CHAR | DOT)* \"]\" ;
 
-STAR_QUANTIFIER = (DOT | CHAR | POS_SET | NEG_SET) \"*\"
-PLUS_QUANTIFIER = (DOT | CHAR | POS_SET | NEG_SET) \"+\"
-QMARK_QUANTIFIER = (DOT | CHAR | POS_SET | NEG_SET) \"?\"
+STAR_QUANTIFIER = (DOT | CHAR | POS_SET | NEG_SET) \"*\" ;
+PLUS_QUANTIFIER = (DOT | CHAR | POS_SET | NEG_SET) \"+\" ;
+QMARK_QUANTIFIER = (DOT | CHAR | POS_SET | NEG_SET) \"?\" ;
 
+NUMBER = (\"1\" | \"2\" | \"3\" | \"4\" | \"5\" | \"6\" | \"7\" | \"8\" | \"9\")
+         (\"1\" | \"2\" | \"3\" | \"4\" | \"5\" | \"6\" | \"7\" | \"8\" | \"9\" | \"0\")* ;
+
+MIN_MAX_QUANTIFIER = (DOT | CHAR | POS_SET | NEG_SET) \"{\" NUMBER \",\" NUMBER \"}\" ;
 "
   )
 
