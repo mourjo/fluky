b391eba2dcdbf470d3f936abf46189f61d8251c2
Mourjo Sen <mourjo@helpshift.com>
2020-11-21 12:52:37 +0530

    Allow meta characters inside []

--- src/fluky/core.clj
+++ src/fluky/core.clj
@@ -7,30 +7,32 @@
   "
 REGEX = (ESCAPED | DOT | CHAR | POS_SET | NEG_SET | STAR_QUANTIFIER | PLUS_QUANTIFIER | QMARK_QUANTIFIER | MIN_MAX_QUANTIFIER)* ;
 
-CHAR = \"a\" | \"A\"| \"b\" | \"B\" | \"c\" | \"C\" | \"d\" | \"D\" | \"e\" | \"E\" | \"f\" | \"F\" |
-       \"g\" | \"G\"| \"h\" | \"H\" | \"i\" | \"I\" | \"j\" | \"J\" | \"k\" | \"K\" | \"l\" | \"L\" |
-       \"m\" | \"M\"| \"n\" | \"N\" | \"o\" | \"O\" | \"p\" | \"P\" | \"q\" | \"Q\" | \"r\" | \"R\" |
-       \"s\" | \"S\"| \"t\" | \"T\" | \"u\" | \"U\" | \"v\" | \"V\" | \"w\" | \"W\" | \"x\" | \"X\" |
-       \"y\" | \"Y\"| \"z\" | \"Z\" | \"0\" | \"1\" | \"2\" | \"3\" | \"4\" | \"5\" | \"6\" | \"7\" |
-       \"8\" | \"9\" ;
+CHAR = 'a' | 'A'| 'b' | 'B' | 'c' | 'C' | 'd' | 'D' | 'e' | 'E' | 'f' | 'F' |
+       'g' | 'G'| 'h' | 'H' | 'i' | 'I' | 'j' | 'J' | 'k' | 'K' | 'l' | 'L' |
+       'm' | 'M'| 'n' | 'N' | 'o' | 'O' | 'p' | 'P' | 'q' | 'Q' | 'r' | 'R' |
+       's' | 'S'| 't' | 'T' | 'u' | 'U' | 'v' | 'V' | 'w' | 'W' | 'x' | 'X' |
+       'y' | 'Y'| 'z' | 'Z' | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' |
+       '8' | '9' ;
 
-BACK_SLASH = \"\\\\\" ;
-ESCAPED = (BACK_SLASH \"+\") | (BACK_SLASH \"*\") | (BACK_SLASH \"?\") | (BACK_SLASH \".\") ;
+META_CHAR = '+' | '-' | '*' | '{' | '}' | '.';
 
-DOT = \".\";
+BACK_SLASH = '\\\\' ;
+ESCAPED = (BACK_SLASH '+') | (BACK_SLASH '-') | (BACK_SLASH '*') | (BACK_SLASH '?') | (BACK_SLASH '.') ;
 
-RANGE = CHAR \"-\" CHAR ;
-POS_SET = \"[\" (RANGE | ESCAPED | CHAR | DOT)* \"]\" ;
-NEG_SET = \"[\" \"^\" (RANGE | ESCAPED | CHAR | DOT)* \"]\" ;
+DOT = '.';
 
-STAR_QUANTIFIER = (DOT | CHAR | POS_SET | NEG_SET) \"*\" ;
-PLUS_QUANTIFIER = (DOT | CHAR | POS_SET | NEG_SET) \"+\" ;
-QMARK_QUANTIFIER = (DOT | CHAR | POS_SET | NEG_SET) \"?\" ;
+RANGE = CHAR '-' CHAR ;
+POS_SET = '[' (RANGE | ESCAPED | CHAR | META_CHAR)* ']' ;
+NEG_SET = '[' '^' (RANGE | ESCAPED | CHAR | META_CHAR)* ']' ;
 
-NUMBER = (\"1\" | \"2\" | \"3\" | \"4\" | \"5\" | \"6\" | \"7\" | \"8\" | \"9\")
-         (\"1\" | \"2\" | \"3\" | \"4\" | \"5\" | \"6\" | \"7\" | \"8\" | \"9\" | \"0\")* ;
+STAR_QUANTIFIER = (DOT | CHAR | POS_SET | NEG_SET) '*' ;
+PLUS_QUANTIFIER = (DOT | CHAR | POS_SET | NEG_SET) '+' ;
+QMARK_QUANTIFIER = (DOT | CHAR | POS_SET | NEG_SET) '?' ;
 
-MIN_MAX_QUANTIFIER = (DOT | CHAR | POS_SET | NEG_SET) \"{\" NUMBER \",\" NUMBER \"}\" ;
+NUMBER = ('1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9')
+         ('1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '0')* ;
+
+MIN_MAX_QUANTIFIER = (DOT | CHAR | POS_SET | NEG_SET) '{' NUMBER ',' NUMBER '}' ;
 "
   )
 
