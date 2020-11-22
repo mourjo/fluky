b006425114c4df541e88aa8a281dacc6918a3710
Mourjo Sen <mourjo@helpshift.com>
2020-11-22 09:26:30 +0530

    Functions that compose for string generations

--- src/fluky/core.clj
+++ src/fluky/core.clj
@@ -13,7 +13,7 @@
   (sort-by int
            [\a \A \b \B \c \C \d \D \e \E \f \F \g \G \h \H \i \I \j \J \k \K \l \L \m \M \n \N \o
             \O \p \P \q \Q \r \R \s \S \t \T \u \U \v \V \w \W \x \X \y \Y \z \Z \0 \1 \2 \3 \4 \5
-            \6 \7 \8 \9 \. \{\} \[\] \ \- \* \+ ]))
+            \6 \7 \8 \9 \. \{ \} \[ \] \  \- \* \+ ]))
 
 (def ^:dynamic *context* :pos)
 
@@ -21,7 +21,12 @@
 
 (defmethod rwalk :REGEX
   [[_ & clauses]]
-  (cstr/join "" (map rwalk clauses)))
+  (reduce (fn [acc typed-f]
+            (if (= :selector (:type typed-f))
+              (str acc (first (filter (:fn typed-f) (shuffle ALPHABET))))
+              (str acc (cstr/join "" ((:fn typed-f) (shuffle ALPHABET))))))
+          ""
+          (map rwalk clauses)))
 
 
 (defmethod rwalk :REGEX_CLAUSE
@@ -31,60 +36,100 @@
 
 (defmethod rwalk :PLUS_QUANTIFIER
   [[_ clause _]]
-  (cstr/join ""
-             (repeatedly (inc (rand-int FACTOR))
-                         (fn [] (rwalk clause)))))
+  {:type :collector
+   :fn (fn [alpha]
+         (let [n (inc (rand-int FACTOR))]
+           (take n (filter (:fn (rwalk clause)) (cycle alpha)))))})
+
+
+(defmethod rwalk :STAR_QUANTIFIER
+  [[_ clause _]]
+  {:type :collector
+   :fn (fn [alpha]
+         (let [n (rand-int FACTOR)]
+           (take n (filter (:fn (rwalk clause)) (cycle alpha)))))})
+
+
+(defmethod rwalk :QMARK_QUANTIFIER
+  [[_ clause _]]
+  {:type :collector
+   :fn (fn [alpha]
+         (let [n (rand-int 2)]
+           (take n (filter (:fn (rwalk clause)) alpha))))})
+
+(defn rand-range
+  [low high]
+  (let [r (rand-int (- (inc high) low))]
+    (+ low r)))
+
+
+(defn parse-number
+  [[_ & digits]]
+  (Long/parseLong (cstr/join "" digits)))
+
+
+(defmethod rwalk :MIN_MAX_QUANTIFIER
+  [[_ clause _ lower _ upper _]]
+  {:type :collector
+   :fn (fn [alpha]
+         (let [n (rand-range (parse-number lower) (parse-number upper))]
+           (take n (filter (:fn (rwalk clause)) (cycle alpha)))))})
+
+
+(defmethod rwalk :DOT
+  [_]
+  {:type :selector
+   :fn (constantly true)})
+
+
+(defmethod rwalk :EXACT_QUANTIFIER
+  [[_ clause _ n _]]
+  {:type :collector
+   :fn (fn [alpha]
+         (take (parse-number n)
+               (filter (:fn (rwalk clause)) (cycle alpha))))})
+
 
 (defmethod rwalk :POS_SET
   [[_ _ & clauses]]
+
   (let [candidates (shuffle (butlast clauses))
-        f (apply some-fn (map rwalk candidates))]
-    (str (first (filter f
-                        (rotate ALPHABET))))))
+        f (apply some-fn (mapv (comp :fn rwalk) candidates))]
+    {:type :selector
+     :fn f}))
 
 
 (defmethod rwalk :NEG_SET
   [[_ _ _ & clauses]]
-  (let [candidates (butlast clauses)]
-    (binding [*context* :neg]
-      (let [f (apply some-fn (map rwalk candidates))]
-        (str (first (remove f
-                            (rotate ALPHABET))))))))
-
-
+  (let [candidates (butlast clauses)
+        f (apply some-fn (map (comp :fn rwalk) candidates))]
+    {:type :selector
+     :fn (complement f)}))
 
 
 (defmethod rwalk :META_CHAR
   [[_ clause]]
-  (if (= :neg *context*)
-    (str (first (filter (fn [c] (not= (first clause) c)) (rotate ALPHABET))))
-    clause))
+  {:type :selector
+   :fn (fn [c] (= (first clause) c))})
 
 
 (defmethod rwalk :CHAR
   [[_ clause]]
-  (if (= :neg *context*)
-    (str (first (filter (fn [c] (not= (first clause) c)) (rotate ALPHABET))))
-    clause))
+  {:type :selector
+   :fn (fn [c] (= (first clause) c))})
 
 (defmethod rwalk :RANGE
   [[_ from _ to :as clauses]]
 
-  (let [f (int (first (binding [*context* :pos] (rwalk from))))
-        t (int (first (binding [*context* :pos] (rwalk to ))))]
-    ;; (prn [f t])
-
-    (if (<= f t)
-      (fn [c] (<= (int f) (int c) (int t)))
-      (throw (ex-info "Invalid range" {:clauses clauses})))
-
-    ;; (if (<= f t)
-    ;;   (if (= :neg *context*)
-    ;;     (str (first (filter (fn [c] (not (<= (int f) (int c) (int t))))
-    ;;                         (rotate ALPHABET))))
-    ;;     (str (char (+ f (rand-int (inc (- t f)))))))
-    ;;   (throw (ex-info "Invalid range" {:clauses clauses})))
-    ))
+  (let [f (rwalk from)
+        t (rwalk to)]
+    {:type :selector
+     :fn (fn [c]
+           (let [starting-char (first (filter (:fn f) ALPHABET))
+                 ending-char (first (filter (:fn t) ALPHABET))]
+             (if (<= (int starting-char) (int ending-char))
+               (<= (int starting-char) (int c) (int ending-char))
+               (throw (ex-info "Invalid range" {:clauses clauses})))))}))
 
 
 (defn random-regex
--- test/fluky/core_test.clj
+++ test/fluky/core_test.clj
@@ -7,37 +7,170 @@
 
 (def regexes
   [["[x-x]+"]
+
    ["[a-x]+"]
+
    ["[a-x{}]+"]
+
    ["a-x"]
+
    ["[a-x+{}]+"]
-   ["[{}]" (fn [all-generated-results]
-             (= #{"{" "}"} (set all-generated-results)))]
-   ["[a-x0-9]+" (fn [all-generated-results]
-                  (some (fn [x]
-                          (re-matches #"[a-x]+" x))
-                        all-generated-results))
+
+   ["[{}]"
+    (fn [all-generated-results]
+      (= #{"{" "}"} (set all-generated-results)))]
+
+   ["[a-x0-9]+"
+    (fn [all-generated-results]
+      (some (fn [x]
+              (re-matches #"[a-x]+" x))
+            all-generated-results))
     (fn [all-generated-results]
       (some (fn [x]
               (re-matches #"[0-9]+" x))
             all-generated-results))]
+
    ["abcd" (fn [all-generated-results]
              (every? #{"abcd"} all-generated-results))]
+
    ["[^x-x]+"]
+
    ["[^a-x]+"]
-   ;;["[^a-x{}]+"]
-   ;;["[^a-x+{}]+"]
-   ["[^{}]" (fn [all-generated-results]
-              (empty? (cset/intersection #{"{" "}"}
-                                         (set all-generated-results))))]
-   ;; java does not negate the whole class!
+
+   ["[^a-x{}]+"]
+
+   ["[^a-x+{}]+"]
+
+   ["[^{}]"
+    (fn [all-generated-results]
+      (empty? (cset/intersection #{"{" "}"}
+                                 (set all-generated-results))))]
+
    ["[^a-x0-9]+" (fn [all-generated-results]
                    (not-any? (fn [x] (re-matches #"[a-x]+" x))
                              all-generated-results))
     (fn [all-generated-results]
       (not-any? (fn [x] (re-matches #"[0-9]+" x))
                 all-generated-results))]
-   ["[^a]+"]])
+
+   ["[^a]+"]
+
+   ["[a]{4}"
+    (fn [all-generated-results]
+      (every? #{"aaaa"}
+              all-generated-results))]
+
+   ["a{4}"
+    (fn [all-generated-results]
+      (every? #{"aaaa"}
+              all-generated-results))]
+
+   ["[^a]{4}"
+    (fn [all-generated-results]
+      (not-any? #{"aaaa"}
+                all-generated-results)
+      (every? (fn [x] (= 4 (count x)))
+              all-generated-results))]
+
+   ["[a]{1,4}"
+    (fn [all-generated-results]
+      (= #{"a" "aa" "aaa" "aaaa"}
+         (set all-generated-results)))]
+
+   ["a{1,4}"
+    (fn [all-generated-results]
+      (= #{"a" "aa" "aaa" "aaaa"}
+         (set all-generated-results)))]
+
+   ["[^a]{1,4}"
+    (fn [all-generated-results]
+      (every? (fn [x] (<= 1 (count x) 4))
+              all-generated-results))]
+
+   ["[a]*"
+    (fn [all-generated-results]
+      (every? (fn [x]
+                (or (= #{\a} (set x))
+                    (empty? x)))
+              all-generated-results))]
+
+   ["a*"
+    (fn [all-generated-results]
+      (every? (fn [x]
+                (or (= #{\a} (set x))
+                    (empty? x)))
+              all-generated-results))]
+
+   ["[a]?"
+    (fn [all-generated-results]
+      (every? (some-fn empty? #{"a"})
+              all-generated-results))]
+
+   ["a?"
+    (fn [all-generated-results]
+      (every? (some-fn empty? #{"a"})
+              all-generated-results))]
+
+   ["[a-e0-2]*"
+    (fn [all-generated-results]
+      (every? (fn [x]
+                (every? #{\a \b \c \d \e \0 \1 \2} x))
+              all-generated-results))]
+
+   ["[a-e0-2]+"
+    (fn [all-generated-results]
+      (every? (fn [x]
+                (and (seq x)
+                     (every? #{\a \b \c \d \e \0 \1 \2} x)))
+              all-generated-results))]
+
+   ["[^a-e0-2]+"
+    (fn [all-generated-results]
+      (every? (fn [x]
+                (and (seq x)
+                     (not-any? #{\a \b \c \d \e \0 \1 \2} x)))
+              all-generated-results))]
+
+   ["[^a-e0-2]{5,5}"
+    (fn [all-generated-results]
+      (every? (fn [x]
+                (and (= 5 (count x))
+                     (not-any? #{\a \b \c \d \e \0 \1 \2} x)))
+              all-generated-results))]
+
+   ["[a-e0-2]{5}"
+    (fn [all-generated-results]
+      (every? (fn [x]
+                (and (= 5 (count x))
+                     (every? #{\a \b \c \d \e \0 \1 \2} x)))
+              all-generated-results))]
+
+   ["[a-e0-2]?"
+    (fn [all-generated-results]
+      (every? (fn [x]
+                (and (<= 0 (count x) 1)
+                     (every? #{\a \b \c \d \e \0 \1 \2} x)))
+              all-generated-results))]
+
+   ["[a-e0-2.]?"
+    (fn [all-generated-results]
+      (every? (fn [x] (<= 0 (count x) 1))
+              all-generated-results))
+    (fn [all-generated-results]
+      (some (fn [x]
+              (not-any? #{\a \b \c \d \e \0 \1 \2} x))
+            all-generated-results))]
+   [".*"]
+
+   ["[.]*"
+    (fn [all-generated-results]
+      (some (fn [x] (every? #{\.} x))
+            all-generated-results))]
+
+   [".{5,6}"
+    (fn [all-generated-results]
+      (every? (fn [x] (<= 5 (count x) 6))
+              all-generated-results))]])
 
 
 (defn valid?
@@ -57,14 +190,14 @@
   (doseq [[regex & additional-validations] regexes]
     (try
       (let [results (atom [])]
-        (dotimes [_ 10]
+        (dotimes [_ 1000]
           (let [result (sut/random-regex regex)]
             (is (valid? regex result))
             (swap! results conj result)))
         (when (seq additional-validations)
           (is ((apply every-pred additional-validations)
                @results)
-              "All additional validations pass on the result")))
+              (str "All additional validations pass on the result " (pr-str regex)))))
       (catch Throwable t
         (println "Erorr in " (pr-str regex))
         (throw t)))))
