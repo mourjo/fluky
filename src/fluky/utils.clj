2e25ec4c638e88441ddac9e005e8dd40833266db
Mourjo Sen <mourjo@helpshift.com>
2020-11-22 16:14:29 +0530

    Add ":" to the list of supported characters

--- src/fluky/utils.clj
+++ src/fluky/utils.clj
@@ -7,7 +7,7 @@
 (let [charset (sort-by int
                        [\a \A \b \B \c \C \d \D \e \E \f \F \g \G \h \H \i \I \j \J \k \K \l \L \m \M \n \N \o
                         \O \p \P \q \Q \r \R \s \S \t \T \u \U \v \V \w \W \x \X \y \Y \z \Z \0 \1 \2 \3 \4 \5
-                        \6 \7 \8 \9 \. \{ \} \[ \] \  \- \* \+ \? \( \) \\])
+                        \6 \7 \8 \9 \. \{ \} \[ \] \  \- \* \+ \? \( \) \\ \:])
       alpha (fn alpha []
               (concat (shuffle charset)
                       (lazy-seq (alpha))))
