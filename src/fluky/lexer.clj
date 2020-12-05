5e7e268b820a655ce061704f221729b0c07d3fcb
Mourjo Sen <mourjo@helpshift.com>
2020-12-05 14:35:51 +0530

    Modified lexer

--- src/fluky/lexer.clj
+++ src/fluky/lexer.clj
@@ -34,60 +34,78 @@
 (defn copy-semi-as-tokens
   [{:keys [tokens semi ctx] :as acc}]
   (-> acc
-      (update :tokens into (mapv (fn [ch] [:char ch]) semi))
+      (update :tokens into (mapv (fn [ch]
+                                   (if (sequential? ch)
+                                     ch
+                                     [:char ch]))
+                                 semi))
       (update :semi empty)))
+
+
+(defn add-type
+  [acc t]
+  (if (:ctx acc)
+    (assoc-in acc [:ctx :type] t)
+    (assoc acc :ctx {:type t})))
 
 (defn tokenize
   [{:keys [tokens semi ctx] :as acc} c]
   (let [result
         (cond
-          (= c \\) (assoc acc :ctx :escaped)
-          (and (not ctx) (= c \?)) (-> acc
-                                       copy-semi-as-tokens
-                                       (update :tokens conj [:qmark]))
-          (and (not ctx) (= c \*)) (-> acc
-                                       copy-semi-as-tokens
-                                       (update :tokens conj [:star]))
-          (and (not ctx) (= c \+)) (-> acc
-                                       copy-semi-as-tokens
-                                       (update :tokens conj [:plus]))
-          (and (not ctx) (= c \.)) (-> acc
-                                       copy-semi-as-tokens
-                                       (update :tokens conj [:dot]))
-          (and (not ctx)
+          (:escaped? ctx) (if (escapeable? c)
+                            (-> acc
+                                (update :semi conj [:escaped c])
+                                (assoc-in [:ctx :escaped?] false))
+                            (throw (ex-info "Unsupported escape sequence"
+                                            {:char c
+                                             :type :lexer-error})))
+          (and (not (:escaped? ctx))
+               (= c \\)) (assoc-in acc [:ctx :escaped?] true)
+          (and (not (:type ctx))
+               (= c \?)) (-> acc
+                             copy-semi-as-tokens
+                             (update :tokens conj [:qmark]))
+          (and (not (:type ctx)) (= c \*)) (-> acc
+                                               copy-semi-as-tokens
+                                               (update :tokens conj [:star]))
+          (and (not (:type ctx))
+               (= c \+)) (-> acc
+                             copy-semi-as-tokens
+                             (update :tokens conj [:plus]))
+          (and (not (:type ctx)) (= c \.)) (-> acc
+                                               copy-semi-as-tokens
+                                               (update :tokens conj [:dot]))
+          (and (not (:type ctx))
                (= c \{)) (-> acc
-               copy-semi-as-tokens
-               (assoc :ctx :quantifier))
-          (and (= ctx :quantifier)
+                             copy-semi-as-tokens
+                             (add-type :quantifier))
+          (and (= (:type ctx) :quantifier)
                (= c \})) (-> acc
-               (update :tokens conj [:quantifier semi])
-               (update :semi empty)
-               (dissoc :ctx))
+                             (update :tokens conj [:quantifier semi])
+                             (update :semi empty)
+                             (update :ctx dissoc :type))
 
-          (and (not ctx) (= c \[)) (-> acc
-                                       copy-semi-as-tokens
-                                       (assoc :ctx :set ))
-          (and (#{:set :neg-set} ctx)
+          (and (not (:type ctx))
+               (= c \[)) (-> acc
+                             copy-semi-as-tokens
+                             (add-type :set))
+          (and (#{:set :neg-set} (:type ctx))
                (= c \])) (-> acc
-               (update :tokens conj [ctx semi])
-               (update :semi empty)
-               (dissoc :ctx))
+                             (update :tokens conj [(:type ctx) semi])
+                             (update :semi empty)
+                             (update :ctx dissoc :type))
 
-          (and (= ctx :set)
+          (and (= (:type ctx) :set)
                (= c \^)
                (empty? semi)) (-> acc
-               (assoc :ctx :neg-set))
+                                  (add-type :neg-set))
 
-          (= :escaped ctx) (if (escapeable? c)
-                             (-> acc
-                                 (update :semi conj c)
-                                 (dissoc :ctx))
-                             (throw (ex-info "Unsupported escape sequence"
-                                             {:char c})))
+
+
 
           :else (-> acc
                     (update :semi conj c)))]
-    ;; (println c "   ->   " (dissoc result :java-pattern-valid?))
+    (println c "   ->   " (dissoc result :java-pattern-valid?))
     result)
   )
 
@@ -100,7 +118,10 @@
                                    :java-pattern-valid? (try (Pattern/compile s) true (catch Exception e false))
                                    :semi [])
                        s)]
-    (if (:ctx result)
-      (throw (ex-info "Invalid regex" {:s s}))
+    (prn result )
+    (if (or (:escaped? (:ctx result))
+            (:type (:ctx result))
+            (seq (remove #{:escaped? :type} (keys (:ctx result)))))
+      (throw (ex-info "Invalid regex" {:s s :type :lexer-error}))
       (:tokens
        (copy-semi-as-tokens result)))))
---
+++ test/fluky/lexer_test.clj
@@ -0,0 +1,79 @@
+(ns fluky.lexer-test
+  (:require [fluky.lexer :as sut]
+            [clojure.test :refer :all])
+  (:import clojure.lang.ExceptionInfo))
+
+
+(defn invalid?
+  [s]
+  (try (sut/lex s)
+       false
+       (catch ExceptionInfo e
+         (-> e
+             ex-data
+             :type
+             (= :lexer-error)))))
+
+
+(deftest lex-test
+  (is (= (sut/lex "[a-z]")
+         [[:set [\a \- \z]]]))
+
+  (is (= (sut/lex "\\\\")
+         [[:escaped \\]]))
+
+  (is (= (sut/lex "a+")
+         [[:char \a] [:plus]]))
+
+  (is (= (sut/lex "a?")
+         [[:char \a] [:qmark]]))
+
+  (is (= (sut/lex "[?*+-]")
+         [[:set [\? \* \+ \-]]]))
+
+  (is (= (sut/lex "[?*+-]*")
+         [[:set [\? \* \+ \-]] [:star]]))
+
+  (is (= (sut/lex "[-+]?[0-9]{1,16}abc*[.*][^0-9]{1,6}[a-z*]+a\\+")
+         [[:set [\- \+]]
+          [:qmark]
+          [:set [\0 \- \9]]
+          [:quantifier [\1 \, \1 \6]]
+          [:char \a]
+          [:char \b]
+          [:char \c]
+          [:star]
+          [:set [\. \*]]
+          [:neg-set [\0 \- \9]]
+          [:quantifier [\1 \, \6]]
+          [:set [\a \- \z \*]]
+          [:plus]
+          [:char \a]
+          [:escaped \+]]))
+
+  (is (= (sut/lex "[a\\-z]")
+         [[:set [\a [:escaped \-] \z]]]))
+
+  (is (= (sut/lex "[a\\-z\\\\]")
+         [[:set [\a [:escaped \-] \z [:escaped \\]]]]))
+
+  (is (= (sut/lex "[a\\-z]")
+         [[:set [\a [:escaped \-] \z]]]))
+
+  (is (= (sut/lex "a-z")
+         [[:char \a] [:char \-] [:char \z]]))
+
+  (is (= (sut/lex "a\\-z")
+         [[:char \a] [:escaped \-] [:char \z]]))
+
+  ;; this is invalid but the lexer does not know that:
+  (is (= (sut/lex "[a-z.]+*")
+         [[:set [\a \- \z \.]] [:plus] [:star]]))
+
+  (is (invalid? "{"))
+  (is (invalid? "["))
+  (is (invalid? "\\8"))
+  (is (invalid? "\\"))
+  (is (invalid? "[a-z\\]"))
+  (is (invalid? "[a-\\z]"))
+  (is (invalid? "[-+]?[0-9]{1,16}abc*[.*][^0-9]{1,6}\\i[a-z*]+a\\+")))
