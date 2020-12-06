6aee8b42bd58720391b64f8766a7b812e5c8584b
Mourjo Sen <mourjo@helpshift.com>
2020-12-06 09:23:32 +0530

    Add new lines between defmethods

--- src/fluky/parser.clj
+++ src/fluky/parser.clj
@@ -16,13 +16,11 @@
   (let [stack (Stack.)]
     (loop [[c & r] chars]
       (cond
-        (not c) ::no-op
+        (not c)
+        ::no-op
 
-
-        (and (= c \-)
-             (not (.isEmpty stack))
-             (seq r)
-             (= :CHAR (first (.peek stack))))
+        (and (= c \-) (not (.isEmpty stack))
+             (seq r) (= :CHAR (first (.peek stack))))
         (let [tos (.pop stack)]
           (when (not (<= (int (second tos)) (int (first r))))
             (throw (ex-info "Invalid range"
@@ -42,29 +40,36 @@
   [processed-tokens [_ chars]]
   (conj processed-tokens (into [:SET] (parse-set chars))))
 
+
 (defmethod parse-token :qmark
   [processed-tokens token]
   (conj processed-tokens {:op (first token)}))
+
 
 (defmethod parse-token :quantifier
   [processed-tokens token]
   (conj processed-tokens {:op (first token)}))
 
+
 (defmethod parse-token :char
   [processed-tokens token]
   (conj processed-tokens [:CHAR (second token)]))
+
 
 (defmethod parse-token :star
   [processed-tokens token]
   (conj processed-tokens {:op (first token)}))
 
+
 (defmethod parse-token :neg-set
   [processed-tokens [_ chars]]
   (conj processed-tokens (into [:NEG_SET] (parse-set chars))))
 
+
 (defmethod parse-token :plus
   [processed-tokens token]
   (conj processed-tokens {:op (first token)}))
+
 
 (defmethod parse-token :escaped
   [processed-tokens token]
